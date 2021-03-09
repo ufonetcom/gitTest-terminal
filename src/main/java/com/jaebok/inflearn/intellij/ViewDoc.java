ff667aa7ffb75ae02a1c7f051d71cbfaef9da58c
ufonetcom <ufozx@naver.com>
2021-03-09 21:26:23 +0900

    github 인텔리제이 연동하기 터미널 제외

--- src/main/java/com/jaebok/inflearn/intellij/ViewDoc.java
+++ src/main/java/com/jaebok/inflearn/intellij/ViewDoc.java
@@ -4,5 +4,6 @@ public class ViewDoc {
     public void viewDoc(){
         double random = Math.random();
         System.out.println("현재줄을 복사합니다.");
+        System.out.println("github 계정 연동 테스트 ");
     }
 }
