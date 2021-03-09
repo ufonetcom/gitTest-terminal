b61fd232e9069241f9afcee7e995293c5f867443
ufonetcom <ufozx@naver.com>
2021-03-09 21:34:05 +0900

    연동하기 터미널 제외 마지막 연동 테스트입니다..

--- src/main/java/com/jaebok/inflearn/intellij/ViewDoc.java
+++ src/main/java/com/jaebok/inflearn/intellij/ViewDoc.java
@@ -5,5 +5,6 @@ public class ViewDoc {
         double random = Math.random();
         System.out.println("현재줄을 복사합니다.");
         System.out.println("github 계정 연동 테스트 ");
+        System.out.println("마지막 계정 연동 테스트");
     }
 }
