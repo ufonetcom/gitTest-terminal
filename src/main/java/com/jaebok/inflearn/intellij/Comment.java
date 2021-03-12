e29bae509e5dcacd6e0db81bbfe5713b27fe43b3
ufonetcom <ufozx@naver.com>
2021-03-12 18:54:16 +0900

    12일 intellij 완강 커밋(마지막)

--- src/main/java/com/jaebok/inflearn/intellij/Comment.java
+++ src/main/java/com/jaebok/inflearn/intellij/Comment.java
@@ -1,11 +1,5 @@
 package com.jaebok.inflearn.intellij;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     private long id;
